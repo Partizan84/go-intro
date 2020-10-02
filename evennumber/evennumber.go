2e2d21794cc69c11a4de38a9467d3b0a69166b59
Partizan84 <knazarov84@gmail.com>
2020-10-02 23:52:15 +0500

    version 1.1

--- evennumber/evennumber.go
+++ evennumber/evennumber.go
@@ -4,10 +4,16 @@ import
 
 "fmt"
 
-"math"
-
 //Функция проверки четности числа.
 func Calculation() {
 
+var number int
+
+fmt.Println("Введите число для проверки на четность:")
+
+fmt.Scanln(number)
+
+	if number % 4 == 0 {
+        fmt.Println("8 делится на 4")
 
 }
--- leonardo-pisano/fibonacci.go
+++ leonardo-pisano/fibonacci.go
@@ -1,2 +1,12 @@
 package pisano
 
+import
+
+"fmt"
+
+"math"
+
+func Calculation() {
+
+	fmt.Println()
+}
