e74f7ceec2d3d316b5a1c1d6929aae4f13971e59
Partizan84 <knazarov84@gmail.com>
2020-10-03 00:30:38 +0500

    EvenNumber change output.

--- evennumber/evennumber.go
+++ evennumber/evennumber.go
@@ -14,8 +14,7 @@ fmt.Println("Введите число для проверки на четнос
 fmt.Scanln(number)
 
 	if number % 4 == 0 {
-		fmt.Println("8 делится на 4")
+		fmt.Println("%n делится на 4", number)
 	}
 		
-
 }
