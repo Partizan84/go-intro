3b88b1277d5093afe946bca035138c2a339f2e73
Partizan84 <knazarov84@gmail.com>
2020-10-03 01:46:31 +0500

    1.3. import in main.

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
+	"fmt"
 
-"fmt"
-
-"github.com/Partizan84/go-intro/homework-2/evennumber"
-
+	"github.com/Partizan84/go-intro/homework-2/evennumber"
 )
 
 func main() {
@@ -13,7 +11,7 @@ func main() {
 	fmt.Println("Задание № 1:")
 
 	evennumber.Calculation()
-	
+
 	fmt.Println("Задание № 2:")
 
 	fmt.Println("Задание № 3:")
