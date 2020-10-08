32422e37ba605c9776e71f58cf5b4a2db7082087
Partizan84 <knazarov84@gmail.com>
2020-10-08 17:32:30 +0500

    Update transport.

--- transport/characteristics.go
+++ transport/characteristics.go
@@ -61,22 +61,42 @@ func Transport() {
 	fmt.Println("Легковой автомобиль:")
 
 	fmt.Println(car1)
+	fmt.Println(car1.brandCar," - марка легкового автомобиля.")
+	fmt.Println(car1.yearCar," - год выпуска легкового автомобиля.")
+	fmt.Println(car1.trunkVolumeCar," - общий объём багажного отделения легкового автомоблия.")
+	fmt.Println(car1.trunkOccupancyCar," - занятный объём багажного отделения легкового автомоблия.")
 	
 	if car1.engineCarOn == false {
 		fmt.Println(car1.brandCar, " двигатель включен.")
 	} else {
 		fmt.Println(car1.brandCar," двигатель включен.")
 	}
+
+	if (car1.windowCar1 == false) && (car1.windowCar2 == false) && (car1.windowCar3 == false) && (car1.windowCar4 == false) {
+		fmt.Println(car1.brandCar," - Все окна закрыты.")
+	} else {
+		fmt.Println(car1.brandCar,"Одно или несколько окон открыты.")
+	}
 	
 	//Грузовой транспорт
 	fmt.Println("Грузовой транспорт:")
 
 	fmt.Println(truck1)
+	fmt.Println(truck1.brandTruck," - марка грузового автомобиля.")
+	fmt.Println(truck1.yearTruck," - год выпуска грузового автомобиля.")
+	fmt.Println(truck1.loadingCapacityTruck," - общий объём кузова грузового автомобиля в килограммах.")
+	fmt.Println(truck1.trunkOccupancyTruck," - занятный объём кузова грузового автомобиля.")
 
 	if truck1.engineCarTruck == false {
 		fmt.Println(truck1.brandTruck," двигатель выключен.")
 	} else {
 		fmt.Println(truck1.brandTruck," двигатель включен.")
+	}
+
+	if (truck1.windowTruck1 == false) && (truck1.windowTruck2 == false) {
+		fmt.Println(truck1.brandTruck," - Все окна закрыты.")
+	} else {
+		fmt.Println(truck1.brandTruck,"Одно или несколько окон открыты.")
 	}
 
 }
