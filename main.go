370e7be36c1f779a28282e68720446ca1bcdd304
Partizan84 <knazarov84@gmail.com>
2020-10-19 18:52:06 +0500

    Update Task.

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"image"
+
+	"fmt"
+	"go-intro\image"
+)
+
+func main() {
+
+	fmt.Println("Задание № 1./n")
+	image.Image()
+
+	fmt.Println("Задание № 2./n")
+
+	fmt.Println("Задание № 3./n")
+}
--- tasktwo/tasktwo.go
+++
@@ -1,6 +0,0 @@
-package tasktwo
-
-func tasktwo() {
-	
-}
-
